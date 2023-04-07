0e179f9e20ee0c28ff76cff6ed90d1515c6e2c2b
NovaVoidHowl <me@novavoidhowl.com>
2023-04-07 22:42:10 +0100

    fix(LHM_Parser): pylint lint cleanup

--- LHM_Parser/LHM_Parser.py
+++ LHM_Parser/LHM_Parser.py
@@ -1,23 +1,44 @@
+"""
+Python library to parse the data from the Libre Hardware Monitor (LHM) json endpoint.
+"""
+
 from datetime import datetime
-import requests
 import json
+import requests
 
 
 class LHM_Parser:
+  """
+  A class to parse the data from the LHM json endpoint.
+
+  Notes
+  -----
+  Pylint C0103: The module name would be too long if full name was used Libre_Hardware_Monitor_Parser
+  Pylint R0904: This is a data source class its meant to have a lots of public methods
+
+
+  Attributes
+  ----------
+  port : int
+    The port of the LHM server.
+  ip : str
+    The ip of the LHM server.
+  raw_data_json : str
+    The raw data from the LHM server.
+  """
 
   def __init__(self, port=8085, ip='127.0.0.1'):
 
     self.port = port
     self.ip = ip
 
-    self.raw_data = {}
-    self.ram_tree = {}
-    self.cpu_tree = {}
-    self.gpu_tree = {}
+    self.__ram_tree = {}
+    self.__cpu_tree = {}
+    self.__gpu_tree = {}
 
     self.update()
 
-  def download_data(self, port=8085, ip='127.0.0.1'):
+  def __download_data(self):
     """
     Download the data from the LHM server.
     """
@@ -25,10 +46,10 @@ class LHM_Parser:
     date_measure = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
     try:
-      response = requests.get(url)
+      response = requests.get(url, timeout=10)
       response.raise_for_status()
 
-      return (response._content, date_measure)
+      return response.content
 
     except requests.exceptions.HTTPError as err:
       print(err)
@@ -39,183 +60,290 @@ class LHM_Parser:
     Update the data from the LHM server.
     """
 
-    data, timestamp = self.download_data()
-    self.raw_data_json = data
-    self.raw_data_dict = json.loads(data)
+    data = self.__download_data()
+    # self.__raw_data_json = data
+    self.__raw_data_dict = json.loads(data)
 
-    self.update_cpu_tree()
-    self.update_gpu_tree()
-    self.update_ram_tree()
+    self.__update_cpu_tree()
+    self.__update_gpu_tree()
+    self.__update_ram_tree()
 
-  def export_as_dict(self):
-    """
-    Export the data as a dictionary.
-    """
-    return self.raw_data_dict
+  # def export_as_dict(self):
+  #   """
+  #   Export the data as a dictionary.
+  #   """
+  #   return self.__raw_data_dict
 
   def system_name(self):
-    return self.raw_data_dict['Children'][0]['Text']
+    """
+    Return the name of the system.
+    """
+    return self.__raw_data_dict['Children'][0]['Text']
 
-  def update_cpu_tree(self):
+  def __update_cpu_tree(self):
     """
     Update the CPU tree.
     """
-    for component in self.raw_data_dict['Children'][0]['Children']:
+    for component in self.__raw_data_dict['Children'][0]['Children']:
       if component['ImageURL'] == 'images_icon/cpu.png':
-        self.cpu_tree.clear()
-        self.cpu_tree = component
+        self.__cpu_tree.clear()
+        self.__cpu_tree = component
 
-  def update_gpu_tree(self):
+  def __update_gpu_tree(self):
     """
     Update the GPU tree.
     """
-    for component in self.raw_data_dict['Children'][0]['Children']:
-      if component['ImageURL'] == 'images_icon/gpu.png':
-        self.gpu_tree.clear()
-        self.gpu_tree = component
+    for component in self.__raw_data_dict['Children'][0]['Children']:
+      # TODO: add support for AMD GPUs
+      if component['ImageURL'] == 'images_icon/nvidia.png':
+        self.__gpu_tree.clear()
+        self.__gpu_tree = component
 
-  def update_ram_tree(self):
+  def __update_ram_tree(self):
     """
     Update the RAM tree.
     """
-    for component in self.raw_data_dict['Children'][0]['Children']:
+    for component in self.__raw_data_dict['Children'][0]['Children']:
       if component['ImageURL'] == 'images_icon/ram.png':
-        self.ram_tree.clear()
-        self.ram_tree = component
+        self.__ram_tree.clear()
+        self.__ram_tree = component
 
 # CPU FUNCTIONS ========================================================================================================
 
   def cpu_name(self):
-    return self.cpu_tree['Text']
+    """
+    Return the name of the CPU.
+    """
+    return self.__cpu_tree['Text']
 
   def cpu_voltage_tree(self):
-    for component in self.cpu_tree['Children']:
+    """
+    Return the CPU voltage tree.
+    """
+    output = None
+    for component in self.__cpu_tree['Children']:
       if component['Text'] == 'Voltages':
-        return component
+        output = component
+    return output
 
   def cpu_power_tree(self):
-    for component in self.cpu_tree['Children']:
+    """
+    Return the CPU power tree.
+    """
+    output = None
+    for component in self.__cpu_tree['Children']:
       if component['Text'] == 'Powers':
-        return component
+        output = component
+    return output
 
   def cpu_clock_tree(self):
-    for component in self.cpu_tree['Children']:
+    """
+    Return the CPU clock tree.
+    """
+    output = None
+    for component in self.__cpu_tree['Children']:
       if component['Text'] == 'Clocks':
-        return component
+        output = component
+    return output
 
   def cpu_temperature_tree(self):
-    for component in self.cpu_tree['Children']:
+    """
+    Return the CPU temperature tree.
+    """
+    output = None
+    for component in self.__cpu_tree['Children']:
       if component['Text'] == 'Temperatures':
-        return component
+        output = component
+    return output
 
   def cpu_load_tree(self):
-    for component in self.cpu_tree['Children']:
+    """
+    Return the CPU load tree.
+    """
+    output = None
+    for component in self.__cpu_tree['Children']:
       if component['Text'] == 'Load':
-        return component
+        output = component
+    return output
 
   def cpu_factor_tree(self):
-    for component in self.cpu_tree['Children']:
+    """
+    Return the CPU factor tree.
+    """
+    output = None
+    for component in self.__cpu_tree['Children']:
       if component['Text'] == 'Factors':
-        return component
+        output = component
+    return output
 
 # GPU FUNCTIONS ========================================================================================================
 
   def gpu_name(self):
-    return self.gpu_tree['Text']
+    """
+    Return the name of the GPU.
+    """
+    return self.__gpu_tree['Text']
 
   def gpu_power_tree(self):
-    for component in self.gpu_tree['Children']:
+    """
+    Return the GPU power tree.
+    """
+    output = None
+    for component in self.__gpu_tree['Children']:
       if component['Text'] == 'Powers':
-        return component
+        output = component
+    return output
 
   def gpu_clock_tree(self):
-    for component in self.gpu_tree['Children']:
+    """
+    Return the GPU clock tree.
+    """
+    output = None
+    for component in self.__gpu_tree['Children']:
       if component['Text'] == 'Clocks':
-        return component
+        output = component
+    return output
 
   def gpu_temperature_tree(self):
-    for component in self.gpu_tree['Children']:
+    """
+    Return the GPU temperature tree.
+    """
+    output = None
+    for component in self.__gpu_tree['Children']:
       if component['Text'] == 'Temperatures':
-        return component
+        output = component
+    return output
 
   def gpu_load_tree(self):
-    for component in self.gpu_tree['Children']:
+    """
+    Return the GPU load tree.
+    """
+    output = None
+    for component in self.__gpu_tree['Children']:
       if component['Text'] == 'Load':
-        return component
+        output = component
+    return output
 
   def gpu_fan_tree(self):
-    for component in self.gpu_tree['Children']:
+    """
+    Return the GPU fan tree.
+    """
+    output = None
+    for component in self.__gpu_tree['Children']:
       if component['Text'] == 'Fans':
-        return component
+        output = component
+    return output
 
   def gpu_controls_tree(self):
-    for component in self.gpu_tree['Children']:
+    """
+    Return the GPU controls tree.
+    """
+    output = None
+    for component in self.__gpu_tree['Children']:
       if component['Text'] == 'Controls':
-        return component
+        output = component
+    return output
 
   def gpu_data_tree(self):
-    for component in self.gpu_tree['Children']:
+    """
+    Return the GPU data tree.
+    """
+    output = None
+    for component in self.__gpu_tree['Children']:
       if component['Text'] == 'Data':
-        return component
+        output = component
+    return output
 
   def gpu_throughput_tree(self):
-    for component in self.gpu_tree['Children']:
+    """
+    Return the GPU throughput tree.
+    """
+    output = None
+    for component in self.__gpu_tree['Children']:
       if component['Text'] == 'Throughput':
-        return component
+        output = component
+    return output
 
 # RAM FUNCTIONS ========================================================================================================
 
   def ram_name(self):
-    return self.ram_tree['Text']
+    """
+    Return the name of the RAM.
+    """
+    return self.__ram_tree['Text']
 
   def ram_load_tree(self):
-    for component in self.ram_tree['Children']:
+    """
+    Return the RAM load tree.
+    """
+    output = None
+    for component in self.__ram_tree['Children']:
       if component['Text'] == 'Load':
-        return component
+        output = component
+    return output
 
   def ram_data_tree(self):
-    for component in self.ram_tree['Children']:
+    """
+    Return the RAM data tree.
+    """
+    output = None
+    for component in self.__ram_tree['Children']:
       if component['Text'] == 'Data':
-        return component
-
+        output = component
+    return output
 
 # DEBUG FUNCTIONS ======================================================================================================
 
-# def print_cpu_tree(self):
-#   print(self.cpu_tree)
+  def print_raw_data(self):
+    """
+    Print the raw dict data.
+    """
+    print(self.__raw_data_dict)
 
-# def print_gpu_tree(self):
-#   print(self.gpu_tree)
+  def print_cpu_tree(self):
+    """
+    Print the CPU tree.
+    """
+    print(self.__cpu_tree)
 
-# def print_ram_tree(self):
-#   print(self.ram_tree)
+  def print_gpu_tree(self):
+    """
+    Print the GPU tree.
+    """
+    print(self.__gpu_tree)
+
+  def print_ram_tree(self):
+    """
+    Print the RAM tree.
+    """
+    print(self.__ram_tree)
 
 # def print_cpu_voltage_tree(self):
-#   for component in self.cpu_tree['Children']:
+#   for component in self.__cpu_tree['Children']:
 #     if component['Text'] == 'Voltages':
 #       print(component)
 
 # def print_cpu_power_tree(self):
-#   for component in self.cpu_tree['Children']:
+#   for component in self.__cpu_tree['Children']:
 #     if component['Text'] == 'Powers':
 #       print(component)
 
 # def print_cpu_clock_tree(self):
-#   for component in self.cpu_tree['Children']:
+#   for component in self.__cpu_tree['Children']:
 #     if component['Text'] == 'Clocks':
 #       print(component)
 
 # def print_cpu_temperature_tree(self):
-#   for component in self.cpu_tree['Children']:
+#   for component in self.__cpu_tree['Children']:
 #     if component['Text'] == 'Temperatures':
 #       print(component)
 
 # def print_cpu_load_tree(self):
-#   for component in self.cpu_tree['Children']:
+#   for component in self.__cpu_tree['Children']:
 #     if component['Text'] == 'Load':
 #       print(component)
 
 # def print_cpu_factor_tree(self):
-#   for component in self.cpu_tree['Children']:
+#   for component in self.__cpu_tree['Children']:
 #     if component['Text'] == 'Factors':
 #       print(component)
